662074456af2755d1be9c2a4bf673b8b172e72b7
jfarzin <76984155+jfarzin@users.noreply.github.com>
2021-05-30 14:52:30 -0700

    generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,25 +1,32 @@
-from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render 
 from django.urls import reverse
-from django.http import HttpResponse
+from django.views import generic
+#from django.http import HttpResponse
 #from django.template import loader
 
 from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    # 3rd try: use 'render' to call request, template; skip loader/httpResponse
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-    
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-    
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class IndexView(generic.ListView):
+    # tutorial part 4: use generic view format
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
